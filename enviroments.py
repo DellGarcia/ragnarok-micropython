585a119322b8a63ca2685d763382617ccbd89ca7
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2023-01-15 13:08:58 -0300

    🔨 Create envs and refactor

---
+++ enviroments.py
@@ -0,0 +1,29 @@
+envs = {
+    "DEBUG_MODE": False,
+    
+    # Infras
+    "left_infra": 34,
+    "center_infra": 35,
+    "right_infra": 19,
+
+    # Ultra
+    "ultra_trigger": 13,
+    "ultra_echo": 12,
+
+    # Rodas
+    "frequency": 1000,
+    
+    "left_pin1": 5,
+    "left_pin2": 18,
+    "left_enable": 32,
+    
+    "right_pin1": 16,
+    "right_pin2": 17,
+    "right_enable": 33,
+    
+    "min_duty": 400,
+    "max_duty": 1023,
+    
+    # Velocidade
+    "speed": 40, 
+}
--- main.py
+++ main.py
@@ -1,66 +1,13 @@
-from machine import Pin, PWM
-from hcsr04 import HCSR04
-from DCMotor import DCMotor
-from time import sleep
-
-left_infra = Pin(34, Pin.IN)
-center_infra = Pin(35, Pin.IN)
-right_infra = Pin(19, Pin.IN)
-
-sensor = HCSR04(trigger_pin=13, echo_pin=12, echo_timeout_us=10000)
- 
-frequency = 1000       
-pin1 = Pin(5, Pin.OUT)    
-pin2 = Pin(18, Pin.OUT)  
-enable1 = PWM(Pin(32), frequency)
-
-pin3 = Pin(16, Pin.OUT)    
-pin4 = Pin(17, Pin.OUT)  
-enable2 = PWM(Pin(33), frequency)
-
-left_motor = DCMotor(pin1, pin2, enable1, min_duty=400, max_duty=1023)
-right_motor = DCMotor(pin3, pin4, enable2, min_duty=400, max_duty=1023)
-
-speed = 40
-back_speed = speed * 3
-
-distance = 0
-
-def left():
-    left_motor.forward(speed)
-    right_motor.backwards(speed)
-    
-def right():
-    right_motor.forward(speed)
-    left_motor.backwards(speed)
-
-def forward():
-    left_motor.forward(speed)
-    right_motor.forward(speed)
-    
-def backward():
-    left_motor.backwards(back_speed)
-    right_motor.backwards(back_speed)
-    
-def stop():
-    left_motor.stop()
-    right_motor.stop()
-    
-def adjust_to_left():
-    while center_infra.value() == 0:
-        left()
-        
-def adjust_to_right():      
-    while center_infra.value() == 0:
-        right()
+from car import left_infra, center_infra, right_infra
+from car import will_collide, display_infra
+from car import adjust_to_left, adjust_to_right, forward, stop
+from car import DEBUG_MODE
 
 while True:
-    # print(left_infra.value(), " | ", center_infra.value(), " | ", right_infra.value())
-    # sleep(0.5)
+    if DEBUG_MODE:
+        display_infra()
     
-    distance = sensor.distance_cm()
-    
-    if distance < 10:
+    if will_collide():
         stop()
     
     leftOn = left_infra.value()
