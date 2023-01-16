b891c9552cacff06e5fdce6026e2fa4870aeea2e
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2023-01-16 09:17:20 -0300

    🔧 added boot files

---
+++ main.py
@@ -0,0 +1,30 @@
+def connectToWifiAndUpdate():
+    import time, machine, network, gc, app.secrets as secrets
+    time.sleep(1)
+    print('Memory free', gc.mem_free())
+
+    from app.ota_updater import OTAUpdater
+
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+    otaUpdater = OTAUpdater('https://github.com/DellGarcia/ragnarok-ugit', main_dir='app', secrets_file="secrets.py")
+    hasUpdated = otaUpdater.install_update_if_available()
+    if hasUpdated:
+        machine.reset()
+    else:
+        del(otaUpdater)
+        gc.collect()
+
+def startApp():
+	from app.main import start
+
+	start()
+
+connectToWifiAndUpdate()
+startApp()
