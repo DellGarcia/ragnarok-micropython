4481d42c32dab387a908a71abf93e023b83ff52a
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2023-01-16 09:15:39 -0300

    add run method

--- app/main.py
+++ app/main.py
@@ -1,26 +1,27 @@
-from car import left_infra, center_infra, right_infra
-from car import will_collide, display_infra
-from car import adjust_to_left, adjust_to_right, forward, stop
-from car import DEBUG_MODE
+def run():
+    from car import left_infra, center_infra, right_infra
+    from car import will_collide, display_infra
+    from car import adjust_to_left, adjust_to_right, forward, stop
+    from car import DEBUG_MODE
 
-while True:
-    if DEBUG_MODE:
-        display_infra()
-    
-    if will_collide():
-        stop()
-    
-    leftOn = left_infra.value()
-    rightOn = right_infra.value()
-    centerOn = center_infra.value()
-    
-    '''
-        Black line equals to 1
-    '''
-    if leftOn == 1:
-        adjust_to_left()
-    elif rightOn == 1:
-        adjust_to_right()
-    else:
-        forward()
-    
+    while True:
+        if DEBUG_MODE:
+            display_infra()
+        
+        if will_collide():
+            stop()
+        
+        leftOn = left_infra.value()
+        rightOn = right_infra.value()
+        centerOn = center_infra.value()
+        
+        '''
+            Black line equals to 1
+        '''
+        if leftOn == 1:
+            adjust_to_left()
+        elif rightOn == 1:
+            adjust_to_right()
+        else:
+            forward()
+        
