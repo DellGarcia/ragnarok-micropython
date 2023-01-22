315321bbd596f098bf9dc476948a71e52f9862e2
Alexandre Ferreira de Lima <alexandre.ferreira1445@gmail.com>
2023-01-22 18:26:01 -0300

    ✨ Print Initial State

--- app/fsm/machine.py
+++ app/fsm/machine.py
@@ -3,6 +3,7 @@ from app.fsm.state import execute_state, transition_to_next_state
 
 
 def init(states, first_state):
+    print('Initial State', first_state)
     set_states(states)
     transition_to_next_state(first_state)
 
