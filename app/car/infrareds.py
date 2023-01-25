0e2ac8369f2f7243cf6729d472452db0a5f4f48b
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2023-01-25 12:18:42 -0300

    :bug: Fix infra order

--- app/car/infrareds.py
+++ app/car/infrareds.py
@@ -13,14 +13,17 @@ for key in infras.keys():
     infras[key] = Pin(envs[key + '_infra'], Pin.IN)
 
 def read_infras():
-    return [infra.value() for infra in infras.values()]
+    return [infras[key].value() for key in ('left', 'center', 'right')]
 
 def display_infra():
-    print(' | '.join(infras.values()))
+    print(infras['left'].value(), ' | ', infras['center'].value() , ' | ', infras['right'].value())
 
 def has_activated(name):
     return get_arg(name + '_on') == 1
 
 def set_infras_arg():
-    for name, value in zip(('left_on', 'center_on', 'right_on'), read_infras()):
-        set_arg(name, value)
+    left_on, center_on, right_on = read_infras()
+    
+    set_arg('left_on', left_on)
+    set_arg('center_on', center_on)
+    set_arg('right_on', right_on)
